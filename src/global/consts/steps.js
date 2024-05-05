46b4d75c296d1ec631238528f016b1c75a89b1db
ilia.brykin <brykin.ilia@gmail.com>
2024-05-05 12:29:44 +0200

    Step 9.1.1.1b

--- src/global/consts/steps.js
+++ src/global/consts/steps.js
@@ -8,4 +8,13 @@ export default {
     classification: 18,
     questions: 32,
   },
+  "9.1.1.1b": {
+    key: "9_1_1_1B",
+    whatChecked: 3,
+    whyChecked: 3,
+    howChecked: 48,
+    sources: 6,
+    classification: 16,
+    questions: 24,
+  },
 };
--- src/i18n/steps/steps_9_1_1/index.js
+++ src/i18n/steps/steps_9_1_1/index.js
@@ -1,10 +1,13 @@
 import step_9_1_1_1a from "./step_9_1_1_1a/index";
+import step_9_1_1_1b from "./step_9_1_1_1b/index";
 
 export default {
   de: {
     ...step_9_1_1_1a.de,
+    ...step_9_1_1_1b.de,
   },
   ru: {
     ...step_9_1_1_1a.ru,
+    ...step_9_1_1_1b.ru,
   },
 };
