35b3474b759b9fb3130bf6c9b6194f881f568cfa
ilia-brykin <ilia-brykin@users.noreply.github.com>
2024-05-10 07:03:57 +0000

    deploy: 9ab0e4e5f52f1d7dd36ce7975e070ef84a74956d

---
+++ chunk.514.3fc4ab6ae5f14a792de1.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkBITV=self.webpackChunkBITV||[]).push([[514],{4314:(n,t,a)=>{a.r(t),a.d(t,{default:()=>s});var e=a(6365);const r={name:"PageStart",components:{ATranslation:a(7197).A}},s=(0,a(6262).A)(r,[["render",function(n,t){const a=(0,e.g2)("a-translation");return(0,e.uX)(),(0,e.CE)("div",null,[(0,e.bF)(a,{tag:"h1",text:"_WELCOME_"})])}]])}}]);
+//# sourceMappingURL=chunk.514.3fc4ab6ae5f14a792de1.js.map
