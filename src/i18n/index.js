c2f707f4cc97efc45f006b26214bbd3c39a036af
Ilia Brykin <brykin.ilia@gmail.com>
2024-05-14 16:13:40 +0200

    Steps 9.1.2

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -5,6 +5,7 @@ import steps_5 from "./steps/steps_5/index";
 import steps_6 from "./steps/steps_6/index";
 import steps_7 from "./steps/steps_7/index";
 import steps_9_1_1 from "./steps/steps_9_1_1/index";
+import steps_9_1_2 from "./steps/steps_9_1_2/index";
 import steps_9_1_3 from "./steps/steps_9_1_3/index";
 import steps_9_1_4 from "./steps/steps_9_1_4/index";
 import steps_9_2_1 from "./steps/steps_9_2_1/index";
@@ -28,6 +29,7 @@ export const de = {
   ...steps_6.de,
   ...steps_7.de,
   ...steps_9_1_1.de,
+  ...steps_9_1_2.de,
   ...steps_9_1_3.de,
   ...steps_9_1_4.de,
   ...steps_9_2_1.de,
@@ -51,6 +53,7 @@ export const ru = {
   ...steps_6.ru,
   ...steps_7.ru,
   ...steps_9_1_1.ru,
+  ...steps_9_1_2.ru,
   ...steps_9_1_3.ru,
   ...steps_9_1_4.ru,
   ...steps_9_2_1.ru,
---
+++ src/i18n/steps/steps_9_1_2/index.js
@@ -0,0 +1,23 @@
+import step_9_1_2_1 from "./step_9_1_2_1/index";
+import step_9_1_2_2 from "./step_9_1_2_2/index";
+import step_9_1_2_3 from "./step_9_1_2_3/index";
+import step_9_1_2_4 from "./step_9_1_2_4/index";
+import step_9_1_2_5 from "./step_9_1_2_5/index";
+
+
+export default {
+  de: {
+    ...step_9_1_2_1.de,
+    ...step_9_1_2_2.de,
+    ...step_9_1_2_3.de,
+    ...step_9_1_2_4.de,
+    ...step_9_1_2_5.de,
+  },
+  ru: {
+    ...step_9_1_2_1.ru,
+    ...step_9_1_2_2.ru,
+    ...step_9_1_2_3.ru,
+    ...step_9_1_2_4.ru,
+    ...step_9_1_2_5.ru,
+  },
+};
