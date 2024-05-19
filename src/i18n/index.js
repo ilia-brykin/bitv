ade3b260060eea483df0c91255a3a664b2249456
ilia.brykin <brykin.ilia@gmail.com>
2024-05-19 13:16:42 +0200

    refactoring translations

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,72 +1,17 @@
 import _de from "./de.json";
 import _ru from "./ru.json";
-
-import steps_5 from "./steps/steps_5/index";
-import steps_6 from "./steps/steps_6/index";
-import steps_7 from "./steps/steps_7/index";
-import steps_9_1_1 from "./steps/steps_9_1_1/index";
-import steps_9_1_2 from "./steps/steps_9_1_2/index";
-import steps_9_1_3 from "./steps/steps_9_1_3/index";
-import steps_9_1_4 from "./steps/steps_9_1_4/index";
-import steps_9_2_1 from "./steps/steps_9_2_1/index";
-import steps_9_2_2 from "./steps/steps_9_2_2/index";
-import steps_9_2_3 from "./steps/steps_9_2_3/index";
-import steps_9_2_4 from "./steps/steps_9_2_4/index";
-import steps_9_2_5 from "./steps/steps_9_2_5/index";
-import steps_9_3_1 from "./steps/steps_9_3_1/index";
-import steps_9_3_2 from "./steps/steps_9_3_2/index";
-import steps_9_3_3 from "./steps/steps_9_3_3/index";
-import steps_9_4_1 from "./steps/steps_9_4_1/index";
-import steps_11_7 from "./steps/steps_11_7/index";
-import steps_11_8 from "./steps/steps_11_8/index";
-import steps_12 from "./steps/steps_12/index";
+import steps from "./steps/index";
 
 import tags from "./tags";
 
 export const de = {
   ..._de,
-  ...steps_5.de,
-  ...steps_6.de,
-  ...steps_7.de,
-  ...steps_9_1_1.de,
-  ...steps_9_1_2.de,
-  ...steps_9_1_3.de,
-  ...steps_9_1_4.de,
-  ...steps_9_2_1.de,
-  ...steps_9_2_2.de,
-  ...steps_9_2_3.de,
-  ...steps_9_2_4.de,
-  ...steps_9_2_5.de,
-  ...steps_9_3_1.de,
-  ...steps_9_3_2.de,
-  ...steps_9_3_3.de,
-  ...steps_9_4_1.de,
-  ...steps_11_7.de,
-  ...steps_11_8.de,
-  ...steps_12.de,
+  ...steps.de,
   ...tags.de,
 };
 
 export const ru = {
   ..._ru,
-  ...steps_5.ru,
-  ...steps_6.ru,
-  ...steps_7.ru,
-  ...steps_9_1_1.ru,
-  ...steps_9_1_2.ru,
-  ...steps_9_1_3.ru,
-  ...steps_9_1_4.ru,
-  ...steps_9_2_1.ru,
-  ...steps_9_2_2.ru,
-  ...steps_9_2_3.ru,
-  ...steps_9_2_4.ru,
-  ...steps_9_2_5.ru,
-  ...steps_9_3_1.ru,
-  ...steps_9_3_2.ru,
-  ...steps_9_3_3.ru,
-  ...steps_9_4_1.ru,
-  ...steps_11_7.ru,
-  ...steps_11_8.ru,
-  ...steps_12.ru,
+  ...steps.ru,
   ...tags.ru,
 };
---
+++ src/i18n/steps/index.js
@@ -0,0 +1,64 @@
+import steps_5 from "./steps_5/index";
+import steps_6 from "./steps_6/index";
+import steps_7 from "./steps_7/index";
+import steps_9_1_1 from "./steps_9_1_1/index";
+import steps_9_1_2 from "./steps_9_1_2/index";
+import steps_9_1_3 from "./steps_9_1_3/index";
+import steps_9_1_4 from "./steps_9_1_4/index";
+import steps_9_2_1 from "./steps_9_2_1/index";
+import steps_9_2_2 from "./steps_9_2_2/index";
+import steps_9_2_3 from "./steps_9_2_3/index";
+import steps_9_2_4 from "./steps_9_2_4/index";
+import steps_9_2_5 from "./steps_9_2_5/index";
+import steps_9_3_1 from "./steps_9_3_1/index";
+import steps_9_3_2 from "./steps_9_3_2/index";
+import steps_9_3_3 from "./steps_9_3_3/index";
+import steps_9_4_1 from "./steps_9_4_1/index";
+import steps_11_7 from "./steps_11_7/index";
+import steps_11_8 from "./steps_11_8/index";
+import steps_12 from "./steps_12/index";
+
+export default {
+  de: {
+    ...steps_5.de,
+    ...steps_6.de,
+    ...steps_7.de,
+    ...steps_9_1_1.de,
+    ...steps_9_1_2.de,
+    ...steps_9_1_3.de,
+    ...steps_9_1_4.de,
+    ...steps_9_2_1.de,
+    ...steps_9_2_2.de,
+    ...steps_9_2_3.de,
+    ...steps_9_2_4.de,
+    ...steps_9_2_5.de,
+    ...steps_9_3_1.de,
+    ...steps_9_3_2.de,
+    ...steps_9_3_3.de,
+    ...steps_9_4_1.de,
+    ...steps_11_7.de,
+    ...steps_11_8.de,
+    ...steps_12.de,
+  },
+  ru: {
+    ...steps_5.ru,
+    ...steps_6.ru,
+    ...steps_7.ru,
+    ...steps_9_1_1.ru,
+    ...steps_9_1_2.ru,
+    ...steps_9_1_3.ru,
+    ...steps_9_1_4.ru,
+    ...steps_9_2_1.ru,
+    ...steps_9_2_2.ru,
+    ...steps_9_2_3.ru,
+    ...steps_9_2_4.ru,
+    ...steps_9_2_5.ru,
+    ...steps_9_3_1.ru,
+    ...steps_9_3_2.ru,
+    ...steps_9_3_3.ru,
+    ...steps_9_4_1.ru,
+    ...steps_11_7.ru,
+    ...steps_11_8.ru,
+    ...steps_12.ru,
+  },
+};
